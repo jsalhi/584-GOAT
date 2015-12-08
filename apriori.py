0c2788955fcedcccca56b94f67389e76e0d0c2bd
Jaleel <jsalhi@umich.edu>
2015-12-07 20:23:51 -0500

    Added ability to create output file for synchronization with algorithm.py + Minor syntactic updates

--- apriori.py
+++ apriori.py
@@ -1,7 +1,8 @@
 import sys
 from itertools import chain, combinations
 from collections import defaultdict
-from optparse import OptionParser
+#from optparse import OptionParser
+import argparse
 
 #Returns items t
 #itemsets = set of all items
@@ -38,7 +39,7 @@ def get_items_and_transactions(lineGen):
         transaction = frozenset(line)
         transactionList.append(transaction)
         for item in transaction:
-            itemSet.add(frozenset([item])) 
+            itemSet.add(frozenset([item]))
     return itemSet, transactionList
 
 #APRIORI algorithm
@@ -60,7 +61,7 @@ def apriori(itemSet, transactionList, minSupport, minConfidence):
 
         Ci = get_supported_items(Li, transactionList, minSupport, itemsetFreqs)
         Li = Ci
-        
+
         k = k + 1
 
     def getSupport(item):
@@ -88,21 +89,6 @@ def apriori(itemSet, transactionList, minSupport, minConfidence):
                         associationRules.append(((tuple(cause), tuple(effect)), confidence))
 
     return supportedItems, associationRules
-
-#Print supported itemsets & association rules
-#
-def print_results(supportedItems, associationRules):
-    #supportedItems: list of tuples of the form ((itemset), support) with min support
-    #
-    for item, support in sorted(supportedItems, key=lambda (item, support): support):
-        print "item: %s , %.3f" % (str(item), support)
-    print "\n------------------------ RULES:"
-    
-    #associationRules: list of tuples in the form (((if), (then)), confidence)
-    #
-    for rule, confidence in sorted(associationRules, key=lambda (rule, confidence): confidence):
-        pre, post = rule
-        print "Rule: %s ==> %s , %.3f" % (str(pre), str(post), confidence)
 
 #Returns a generator that outputs a frozenset() representing each transaction
 #
@@ -137,34 +123,87 @@ def get_transaction_gen_from_files(filenames, window_size):
 
 
 def parse_options():
-    optparser = OptionParser()
-    optparser.add_option('-f', '--inputFile',
+    optparser = argparse.ArgumentParser()
+    #optparser = OptionParser()
+    optparser.add_argument('-f', '--inputFile',
                          dest='input',
-                         help='filename containing csv',
+                         help='Single filename for APRIORI',
                          default=None)
-    optparser.add_option('-s', '--minSupport',
+    optparser.add_argument('-l', '--fileList',
+                        dest='fileList',
+                        help='Filenames containing APRIORI training data',
+                        nargs='+',
+                        default=None)
+    optparser.add_argument('-s', '--minSupport',
                          dest='minS',
-                         help='minimum support value',
+                         help='Min support value',
                          default=0.15,
-                         type='float')
-    optparser.add_option('-c', '--minConfidence',
+                         type=float)
+    optparser.add_argument('-c', '--minConfidence',
                          dest='minC',
-                         help='minimum confidence value',
+                         help='Min confidence value',
                          default=0.6,
-                         type='float')
+                         type=float)
+    optparser.add_argument('-w', '--windowSize',
+                         dest='windowSize',
+                         help='Window size for apriori',
+                         default=3,
+                         type=int)
     return optparser.parse_args()
 
-if __name__ == "__main__":
-    (options, args) = parse_options()    
-    infileGen = None
-    if options.input is not None:
-        infileGen = get_transaction_generator(options.input)
-    else:
-        print 'No input file specified\n'
-        sys.exit(0)
-    minSupport = options.minS
-    minConfidence = options.minC
+#Output to file & association rules
+#
+def write_results(outfile_name, associationRules):
+    with open(outfile_name, 'w') as f:
+        for rule, confidence in associationRules:
+            cause, effect = rule
+            if len(cause) > 1:
+                continue
 
-    itemSet, transactionList = get_items_and_transactions(infileGen)
-    supportedItems, associationRules = apriori(itemSet, transactionList, minSupport, minConfidence)
-    print_results(supportedItems, associationRules)
+            wholeset = set(cause + effect)
+            rule_size = len(wholeset)
+
+            f.write(str(rule_size) + "\n")
+            f.write(str(cause[0]) + "\n")
+            for associated_element in set(effect):
+                f.write(str(associated_element) + "\n")
+
+
+#Print supported itemsets & association rules
+#
+def print_results(supportedItems, associationRules):
+        #supportedItems: list of tuples of the form ((itemset), support) with min support
+        #
+        for item, support in sorted(supportedItems, key=lambda (item, support): support):
+            print "item: %s , %.3f" % (str(item), support)
+
+        print "\n------------------------ RULES:"
+        #associationRules: list of tuples in the form (((if), (then)), confidence)
+        #
+        for rule, confidence in sorted(associationRules, key=lambda (rule, confidence): confidence):
+            pre, post = rule
+            print "Rule: %s ==> %s , %.3f" % (str(pre), str(post), confidence)
+
+
+if __name__ == "__main__":
+    options = parse_options()
+    infile_gen = None
+    if options.input is None and options.fileList is None:
+        print "No input file(s) specified. Exiting"
+        sys.exit(0)
+
+    if options.input is not None:
+        #infile_gen = get_transaction_generator(options.input)
+        infile_gen = get_transaction_gen_from_files(["EXAMPLE.txt"], 4)
+    if options.fileList is not None:
+        print options.fileList
+        infile_gen = get_transaction_gen_from_files(options.fileList, options.windowSize)
+
+    min_support = options.minS
+    min_confidence = options.minC
+
+    item_set, transaction_list = get_items_and_transactions(infile_gen)
+    supported_items, association_rules = apriori(item_set, transaction_list, min_support, min_confidence)
+
+    write_results("apriori_out.txt", association_rules)
+    print_results(supported_items, association_rules)
