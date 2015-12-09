9b3452c691d80f5a677e631b293e28cc95e9fe13
Alex <alancast@umich.edu>
2015-12-09 14:39:35 -0500

    updated testing script

--- run_tests.sh
+++ run_tests.sh
@@ -2,8 +2,8 @@
 infile="$1"
 
 time python algorithm.py $infile
-echo "TIMING TEST DONE FOR PROPRIETARY ALGO"
+echo TIMING TEST DONE FOR PROPRIETARY ALGO >&2
 time python NaiveTime.py $infile
-echo "TIMING TEST DONE FOR NAIVE ALGO"
+echo TIMING TEST DONE FOR NAIVE ALGO >&2
 time python NormalTime.py $infile
-echo "TIMING TEST DONE FOR NORMAL ALGO"
+echo TIMING TEST DONE FOR NORMAL ALGO >&2
